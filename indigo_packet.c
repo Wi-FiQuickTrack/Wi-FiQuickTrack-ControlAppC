d0628c8b00ef4aa26e02e1169aa6594773cb9b78
WFA-rtsao <rtsao@wi-fi.org>
2020-12-16 10:59:34 +0800

    Refine indigo_packet.c and add more comments

--- indigo_packet.c
+++ indigo_packet.c
@@ -28,17 +28,20 @@
 #include "indigo_api.h"
 #include "utils.h"
 
-int capture_packet = 0, capture_count = 0;
-int debug_packet = 0;
+int capture_packet = 0, capture_count = 0;  /* debug. Write the received packets to files */
+int debug_packet = 0;                       /* used by the packet hexstring print */
 
+/* Parse the Indigo message from the packet to the wrapper */
 int parse_packet(struct packet_wrapper *req, char *packet, int packet_len) {
-    int parser = 0, ret, i;
-    struct indigo_api *api;
-    struct indigo_tlv *tlv;
+    int i = 0, parser = 0, ret = 0;
+    struct indigo_api *api = NULL;
+    struct indigo_tlv *tlv = NULL;
 
+    /* Print the debug message */
     if (debug_packet)
         print_hex(packet, packet_len);
 
+    /* Parse the message header */
     ret = parse_message_hdr(&req->hdr, packet, packet_len);
     if (ret > 0) {
         if (debug_packet) {
@@ -49,6 +52,7 @@ int parse_packet(struct packet_wrapper *req, char *packet, int packet_len) {
         return -1;
     }
 
+    /* Parse the TLVs */
     while (packet_len - parser > 0) {
         req->tlv[req->tlv_num] = (struct tlv_hdr *)malloc(sizeof(struct tlv_hdr));
         memset(req->tlv[req->tlv_num], 0, sizeof(struct tlv_hdr));
@@ -66,7 +70,7 @@ int parse_packet(struct packet_wrapper *req, char *packet, int packet_len) {
         }
     }
 
-    /* redundant when enable debug_packet */
+    /* Redundant when enable debug_packet */
     if (!debug_packet) {
         api = get_api_by_id(req->hdr.type);
         if (api) {
@@ -87,6 +91,7 @@ int parse_packet(struct packet_wrapper *req, char *packet, int packet_len) {
         }
     }
 
+    /* Only for the debug purpose. Write the received packet to files */
     if (capture_packet) {
         char fn[S_BUFFER_LEN], value[8];
         char *buffer;
@@ -112,8 +117,9 @@ int parse_packet(struct packet_wrapper *req, char *packet, int packet_len) {
     return 0;
 }
 
+/* Find the specific TLV by TLV ID from the wrapper */
 struct tlv_hdr *find_wrapper_tlv_by_id(struct packet_wrapper *wrapper, int id) {
-    int i;
+    int i = 0;
 
     for (i = 0; i < TLV_NUM; i++) {
         if (wrapper->tlv[i]) {
@@ -126,8 +132,9 @@ struct tlv_hdr *find_wrapper_tlv_by_id(struct packet_wrapper *wrapper, int id) {
     return NULL;
 }
 
+/* Free the wrapper malloc's memory */
 int free_packet_wrapper(struct packet_wrapper *wrapper) {
-    int i;
+    int i = 0;
 
     for (i = 0; i < TLV_NUM; i++) {
         if (wrapper->tlv[i]) {
@@ -139,6 +146,7 @@ int free_packet_wrapper(struct packet_wrapper *wrapper) {
     return 0;
 }
 
+/* Parse the message header */
 int parse_message_hdr(struct message_hdr *hdr, char *message, int message_len) {
     if (message_len < sizeof(struct message_hdr)) {
         return -1;
@@ -153,6 +161,7 @@ int parse_message_hdr(struct message_hdr *hdr, char *message, int message_len) {
     return sizeof(struct message_hdr);
 }
 
+/* Convert the packet message header from the structure */
 int gen_message_hdr(char *message, int message_len, struct message_hdr *hdr) {
     int len = 0;
 
@@ -171,14 +180,16 @@ int gen_message_hdr(char *message, int message_len, struct message_hdr *hdr) {
     return sizeof(struct message_hdr);
 }
 
+/* Print the header */
 void print_message_hdr(struct message_hdr *hdr) {
     indigo_logger(LOG_LEVEL_INFO, "Version: %d", hdr->version);
     indigo_logger(LOG_LEVEL_INFO, "Type: 0x%04x (%s)", hdr->type, get_api_type_by_id(hdr->type));
-    indigo_logger(LOG_LEVEL_INFO, "Seqence: 0x%04x", hdr->seq);
+    indigo_logger(LOG_LEVEL_INFO, "Sequence: 0x%04x", hdr->seq);
     indigo_logger(LOG_LEVEL_INFO, "Reserved: 0x%02x", hdr->reserved);
     indigo_logger(LOG_LEVEL_INFO, "Reserved2: 0x%02x", hdr->reserved2);
 }
 
+/* Print the hexstring of the specific range */
 int print_hex(char *message, int message_len) {
     int i;
     for(i = 0; i < message_len; i++)  {
@@ -188,6 +199,7 @@ int print_hex(char *message, int message_len) {
     return 0;
 }
 
+/* Add the TLV to the wrapper */
 int add_wrapper_tlv(struct packet_wrapper *wrapper, int id, int len, char *value) {
     if (add_tlv(wrapper->tlv[wrapper->tlv_num], id, len, value) == 0) {
         wrapper->tlv_num++;
@@ -196,6 +208,7 @@ int add_wrapper_tlv(struct packet_wrapper *wrapper, int id, int len, char *value
     return 1;
 }
 
+/* Fill the TLV with the ID, length and value */
 int add_tlv(struct tlv_hdr *tlv, int id, int len, char *value) {
     if (!tlv)
         return 1;
@@ -206,6 +219,7 @@ int add_tlv(struct tlv_hdr *tlv, int id, int len, char *value) {
     return 0;
 }
 
+/* Parse the TLV from the packet to the structure */
 int parse_tlv(struct tlv_hdr *tlv, char *packet, int packet_len) {
     if (packet_len < 3) {
         return -1;
@@ -219,6 +233,7 @@ int parse_tlv(struct tlv_hdr *tlv, char *packet, int packet_len) {
     return tlv->len+3;
 }
 
+/* Convert the TLV structure to the packet */
 int gen_tlv(char *packet, int packet_size, struct tlv_hdr *t) {
     int len = 0;
 
@@ -235,15 +250,18 @@ int gen_tlv(char *packet, int packet_size, struct tlv_hdr *t) {
     return len;
 }
 
+/* Print the TLV */
 void print_tlv(struct tlv_hdr *t) {
-    int i;
+    int i = 0;
     char buffer[S_BUFFER_LEN], value[S_BUFFER_LEN];
     struct indigo_tlv *tlv = get_tlv_by_id(t->id);
+
+    memset(buffer, 0, sizeof(buffer));
+    memset(value, 0, sizeof(value));
 
     indigo_logger(LOG_LEVEL_INFO, "    ID: 0x%04x (%s)", t->id, tlv == NULL ? "Unknown" : tlv->name);
     indigo_logger(LOG_LEVEL_INFO, "    Length: %d", t->len);
 
-    memset(buffer, 0, sizeof(buffer));
     if (t->len > 0) {
         sprintf(buffer, "    Value: ");
     }
@@ -254,8 +272,9 @@ void print_tlv(struct tlv_hdr *t) {
     indigo_logger(LOG_LEVEL_INFO, buffer);
 }
 
+/* Convert the wrapper to the packet includes the message header and all TLVs. Used by the ACK and resposne */
 int assemble_packet(char *packet, int packet_size, struct packet_wrapper *wrapper) {
-    int packet_len = 0, i, ret;
+    int i = 0, ret = 0, packet_len = 0;
 
     ret = gen_message_hdr(packet, packet_size, &wrapper->hdr);
     packet_len += ret;
