7d84330f3e4df89eb1999e30b9cf3b2a52b8c9d5
WFA-rtsao <rtsao@wi-fi.org>
2021-01-08 15:06:53 +0800

    Implement the script to generate the deb file.

---
+++ build_deb.sh
@@ -0,0 +1,91 @@
+#!bin/bash
+
+package_name="WFA-Indigo-ControlAppC"
+version=""
+git_revision=""
+deb_name=${package_name}.deb
+control_file=${package_name}/DEBIAN/control
+postinst_file=${package_name}/DEBIAN/postinst
+prerm_file=${package_name}/DEBIAN/prerm
+postrm_file=${package_name}/DEBIAN/postrm
+source_folder=${package_name}/usr/local/bin/${package_name}/source
+installed_source_folder=/usr/local/bin/${package_name}/source
+
+create_source_folder() {
+    rm -rf ${package_name}
+    mkdir -p ${package_name}/DEBIAN
+    mkdir -p ${source_folder}
+}
+
+copy_filter_source() {
+    cp -rf *.c *.h Makefile ${source_folder}
+}
+
+create_control() {
+    echo "Package: ${package_name}" >"$control_file"
+    echo "Version: ${version}-${revision}" >>"$control_file"
+    echo "Architecture: all" >>"$control_file"
+    echo "Depends: build-essential" >>"$control_file"
+    echo "Essential: no" >>"$control_file"
+    echo "Priority: optional" >>"$control_file"
+    echo "Maintainer: Wi-Fi Alliance" >>"$control_file"
+    echo "Description: This Software is to control the DUT and test platform" >>"$control_file"
+    echo "" >>"$control_file"
+}
+
+create_postinst() {
+    echo "#!/bin/bash" >"$postinst_file"
+    echo "echo \"Start the installation and compile the source code.\"" >>"$postinst_file"
+    echo "cd ${installed_source_folder}" >>"$postinst_file"
+    echo "sed -i 's/VERSION = /VERSION = \"${version}\"#VERSION = /' Makefile" >>"$postinst_file"
+    echo "make clean >/dev/null" >>"$postinst_file"
+    echo "make >/dev/null" >>"$postinst_file"
+    echo "cp app ../app_dut" >>"$postinst_file"
+    echo "make clean >/dev/null" >>"$postinst_file"
+
+    echo "sed -i 's/ROLE = dut/ROLE = tp/' Makefile" >>"$postinst_file"
+    echo "make >/dev/null" >>"$postinst_file"
+    echo "cp app ../app_tp" >>"$postinst_file"
+    echo "make clean >/dev/null" >>"$postinst_file"
+
+    echo "echo \"Test application version\"" >>"$postinst_file"
+    echo "../app_dut -v" >>"$postinst_file"
+    echo "../app_tp -v" >>"$postinst_file"
+    echo "echo \"\"" >>"$postinst_file"
+    echo "echo \"Complete the installation. If you would like to modify the source code for the platform-specific change, you can go to ${installed_source_folder}\"" >>"$postinst_file"
+    chmod 755 "$postinst_file"
+}
+
+create_prerm() {
+    echo "#!/bin/bash" >"$prerm_file"
+    echo "sudo killall app_dut >/dev/null 2>/dev/null" >>"$prerm_file"
+    echo "sudo killall app_tp >/dev/null 2>/dev/null" >>"$prerm_file"
+    echo "sleep 3" >>"$prerm_file"
+    echo "cd ${installed_source_folder}" >>"$prerm_file"
+    echo "rm -rf /usr/local/bin/${package_name}/app_dut" >>"$prerm_file"
+    echo "rm -rf /usr/local/bin/${package_name}/app_tp" >>"$prerm_file"
+    chmod 755 "$prerm_file"
+}
+
+create_deb() {
+    dpkg -b ${package_name} >/dev/null
+}
+
+if [ -z "$1" ]
+then
+    echo "Please specify the version. E.g., 1.0.10"
+    exit
+else
+    version="$1"
+    revision=`git rev-parse --short HEAD`
+fi
+create_source_folder
+create_control
+copy_filter_source
+create_postinst
+create_prerm
+create_deb
+
+echo "Complete. Please reference to the following usage."
+echo "1. Please remove the package by \"sudo apt remove wfa-indigo-controlappc\""
+echo "2. Please install the package by \"sudo apt install ./WFA-Indigo-ControlAppC.deb\""
