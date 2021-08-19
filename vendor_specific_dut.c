6f529b3d6bc993771e1b91b4934c45bc4b68ecee
WFA-achuang <achuang@wi-fi.org>
2021-08-19 11:25:16 +0800

    Update WFA license

--- vendor_specific_dut.c
+++ vendor_specific_dut.c
@@ -1,14 +1,8 @@
 /* Copyright (c) 2020 Wi-Fi Alliance                                                */
-/* Permission to a personal, non-exclusive, non-transferable use of this            */
-/* software in object code form only, solely for the purposes of supporting         */
-/* Wi-Fi certification program development, Wi-Fi pre-certification testing,        */
-/* and formal Wi-Fi certification testing by authorized test labs, Wi-Fi            */
-/* Alliance members in good standing and their customers, provided that any         */
-/* part of this software shall not be copied or reproduced in any way. Wi-Fi        */
-/* Alliance Software License Agreement governing this software can be found at      */
-/* https://www.wi-fi.org/file/wi-fi-alliance-software-end-user-license-agreement.   */
-/* The foregoing license shall terminate if Customer breaches any term hereof       */
-/* and fails to cure such breach within five (5) days of notice of breach.          */
+
+/* Permission to use, copy, modify, and/or distribute this software for any         */
+/* purpose with or without fee is hereby granted, provided that the above           */
+/* copyright notice and this permission notice appear in all copies.                */
 
 /* THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL                    */
 /* WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED                    */
