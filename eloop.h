db7e608f7684228f8a5eee8e799def4278e508c7
changename <change@change.com>
2023-05-30 02:17:55 +0530

    Fix eloop namespace
    eloop is used by both hostap and QT-APP, as C doesn't have namespaces,
    prefix all QT APIs with qt_. This is needed for those embedded build
    systems that compile both hostap + QT.

--- eloop.h
+++ eloop.h
@@ -23,21 +23,21 @@
 #ifndef ELOOP_H
 #define ELOOP_H
 
-/* Magic number for eloop_cancel_timeout() */
+/* Magic number for qt_eloop_cancel_timeout() */
 #define ELOOP_ALL_CTX (void *) -1
 
 /**
- * eloop_init() - Initialize global event loop data
+ * qt_eloop_init() - Initialize global event loop data
  * @user_data: Pointer to global data passed as eloop_ctx to signal handlers
  *
  * This function must be called before any other eloop_* function. user_data
  * can be used to configure a global (to the process) pointer that will be
  * passed as eloop_ctx parameter to signal handlers.
  */
-void eloop_init(void *user_data);
+void qt_eloop_init(void *user_data);
 
 /**
- * eloop_register_read_sock - Register handler for read events
+ * qt_eloop_register_read_sock - Register handler for read events
  * @sock: File descriptor number for the socket
  * @handler: Callback function to be called when data is available for reading
  * @eloop_data: Callback context data (eloop_ctx)
@@ -48,22 +48,22 @@ void eloop_init(void *user_data);
  * function will be called whenever data is available for reading from the
  * socket.
  */
-int eloop_register_read_sock(int sock,
+int qt_eloop_register_read_sock(int sock,
 			     void (*handler)(int sock, void *eloop_ctx,
 					     void *sock_ctx),
 			     void *eloop_data, void *user_data);
 
 /**
- * eloop_unregister_read_sock - Unregister handler for read events
+ * qt_eloop_unregister_read_sock - Unregister handler for read events
  * @sock: File descriptor number for the socket
  *
  * Unregister a read socket notifier that was previously registered with
- * eloop_register_read_sock().
+ * qt_eloop_register_read_sock().
  */
-void eloop_unregister_read_sock(int sock);
+void qt_eloop_unregister_read_sock(int sock);
 
 /**
- * eloop_register_timeout - Register timeout
+ * qt_eloop_register_timeout - Register timeout
  * @secs: Number of seconds to the timeout
  * @usecs: Number of microseconds to the timeout
  * @handler: Callback function to be called when timeout occurs
@@ -74,26 +74,26 @@ void eloop_unregister_read_sock(int sock);
  * Register a timeout that will cause the handler function to be called after
  * given time.
  */
-int eloop_register_timeout(unsigned int secs, unsigned int usecs,
+int qt_eloop_register_timeout(unsigned int secs, unsigned int usecs,
 			   void (*handler)(void *eloop_ctx, void *timeout_ctx),
 			   void *eloop_data, void *user_data);
 
 /**
- * eloop_cancel_timeout - Cancel timeouts
+ * qt_eloop_cancel_timeout - Cancel timeouts
  * @handler: Matching callback function
  * @eloop_data: Matching eloop_data or %ELOOP_ALL_CTX to match all
  * @user_data: Matching user_data or %ELOOP_ALL_CTX to match all
  * Returns: Number of cancelled timeouts
  *
  * Cancel matching <handler,eloop_data,user_data> timeouts registered with
- * eloop_register_timeout(). ELOOP_ALL_CTX can be used as a wildcard for
+ * qt_eloop_register_timeout(). ELOOP_ALL_CTX can be used as a wildcard for
  * cancelling all timeouts regardless of eloop_data/user_data.
  */
-int eloop_cancel_timeout(void (*handler)(void *eloop_ctx, void *sock_ctx),
+int qt_eloop_cancel_timeout(void (*handler)(void *eloop_ctx, void *sock_ctx),
 			 void *eloop_data, void *user_data);
 
 /**
- * eloop_register_signal - Register handler for signals
+ * qt_eloop_register_signal - Register handler for signals
  * @sig: Signal number (e.g., SIGHUP)
  * @handler: Callback function to be called when the signal is received
  * @user_data: Callback context data (signal_ctx)
@@ -106,47 +106,46 @@ int eloop_cancel_timeout(void (*handler)(void *eloop_ctx, void *sock_ctx),
  *
  * Signals are 'global' events and there is no local eloop_data pointer like
  * with other handlers. The global user_data pointer registered with
- * eloop_init() will be used as eloop_ctx for signal handlers.
+ * qt_eloop_init() will be used as eloop_ctx for signal handlers.
  */
-int eloop_register_signal(int sig,
+int qt_eloop_register_signal(int sig,
 			  void (*handler)(int sig, void *eloop_ctx,
 					  void *signal_ctx),
 			  void *user_data);
-
 /**
- * eloop_run - Start the event loop
+ * qt_eloop_run - Start the event loop
  *
  * Start the event loop and continue running as long as there are any
  * registered event handlers. This function is run after event loop has been
  * initialized with event_init() and one or more events have been registered.
  */
-void eloop_run(void);
+void qt_eloop_run(void);
 
 /**
- * eloop_terminate - Terminate event loop
+ * qt_eloop_terminate - Terminate event loop
  *
  * Terminate event loop even if there are registered events. This can be used
  * to request the program to be terminated cleanly.
  */
-void eloop_terminate(void);
+void qt_eloop_terminate(void);
 
 /**
- * eloop_destroy - Free any resources allocated for the event loop
+ * qt_eloop_destroy - Free any resources allocated for the event loop
  *
  * After calling eloop_destoy(), other eloop_* functions must not be called
- * before re-running eloop_init().
+ * before re-running qt_eloop_init().
  */
-void eloop_destroy(void);
+void qt_eloop_destroy(void);
 
 /**
- * eloop_terminated - Check whether event loop has been terminated
+ * qt_eloop_terminated - Check whether event loop has been terminated
  * Returns: 1 = event loop terminate, 0 = event loop still running
  *
- * This function can be used to check whether eloop_terminate() has been called
+ * This function can be used to check whether qt_eloop_terminate() has been called
  * to request termination of the event loop. This is normally used to abort
- * operations that may still be queued to be run when eloop_terminate() was
+ * operations that may still be queued to be run when qt_eloop_terminate() was
  * called.
  */
-int eloop_terminated(void);
+int qt_eloop_terminated(void);
 
 #endif /* ELOOP_H */
--- main.c
+++ main.c
@@ -72,7 +72,7 @@ static int control_socket_init(int port) {
     }
 
     /* Register to eloop and ready for the socket event */
-    if (eloop_register_read_sock(s, control_receive_message, NULL, NULL)) {
+    if (qt_eloop_register_read_sock(s, control_receive_message, NULL, NULL)) {
         indigo_logger(LOG_LEVEL_ERROR, "Failed to initiate ControlAppC");
         close(s);
         return -1;
@@ -115,7 +115,7 @@ static void control_receive_message(int sock, void *eloop_ctx, void *sock_ctx) {
         fill_wrapper_ack(&resp, req.hdr.seq, 0x31, "Unable to parse the packet");
         len = assemble_packet(buffer, BUFFER_LEN, &resp);
 
-        sendto(sock, (const char *)buffer, len, MSG_CONFIRM, (const struct sockaddr *) &from, fromlen); 
+        sendto(sock, (const char *)buffer, len, MSG_CONFIRM, (const struct sockaddr *) &from, fromlen);
         goto done;
     }
 
@@ -127,7 +127,7 @@ static void control_receive_message(int sock, void *eloop_ctx, void *sock_ctx) {
         indigo_logger(LOG_LEVEL_ERROR, "API Unknown (0x%04x): No registered handler", req.hdr.type);
         fill_wrapper_ack(&resp, req.hdr.seq, 0x31, "Unable to find the API handler");
         len = assemble_packet(buffer, BUFFER_LEN, &resp);
-        sendto(sock, (const char *)buffer, len, MSG_CONFIRM, (const struct sockaddr *) &from, fromlen); 
+        sendto(sock, (const char *)buffer, len, MSG_CONFIRM, (const struct sockaddr *) &from, fromlen);
         goto done;
     }
 
@@ -142,7 +142,7 @@ static void control_receive_message(int sock, void *eloop_ctx, void *sock_ctx) {
         indigo_logger(LOG_LEVEL_ERROR, "API %s: Failed to verify and return NACK", api->name);
         fill_wrapper_ack(&resp, req.hdr.seq, 1, "Unable to find the API handler");
         len = assemble_packet(buffer, BUFFER_LEN, &resp);
-        sendto(sock, (const char *)buffer, len, MSG_CONFIRM, (const struct sockaddr *) &from, fromlen); 
+        sendto(sock, (const char *)buffer, len, MSG_CONFIRM, (const struct sockaddr *) &from, fromlen);
         goto done;
     }
 
@@ -151,7 +151,7 @@ static void control_receive_message(int sock, void *eloop_ctx, void *sock_ctx) {
     if (api->handle && api->handle(&req, &resp) == 0) {
         indigo_logger(LOG_LEVEL_INFO, "API %s: Return execution result", api->name);
         len = assemble_packet(buffer, BUFFER_LEN, &resp);
-        sendto(sock, (const char *)buffer, len, MSG_CONFIRM, (const struct sockaddr *) &from, fromlen); 
+        sendto(sock, (const char *)buffer, len, MSG_CONFIRM, (const struct sockaddr *) &from, fromlen);
     } else {
         indigo_logger(LOG_LEVEL_DEBUG, "API %s (0x%04x): No handle function", api ? api->name : "Unknown", req.hdr.type);
     }
@@ -270,7 +270,7 @@ static void handle_term(int sig, void *eloop_ctx, void *signal_ctx) {
     indigo_logger(LOG_LEVEL_INFO, "Signal %d received - terminating\n", sig);
     (void) eloop_ctx;
     (void) signal_ctx;
-    eloop_terminate();
+    qt_eloop_terminate();
     vendor_deinit();
 }
 
@@ -323,22 +323,22 @@ int main(int argc, char* argv[]) {
     vendor_init();
 
     /* Start eloop */
-    eloop_init(NULL);
+    qt_eloop_init(NULL);
 
     /* Register SIGTERM */
-    eloop_register_signal(SIGINT, handle_term, NULL);
-    eloop_register_signal(SIGTERM, handle_term, NULL);
+    qt_eloop_register_signal(SIGINT, handle_term, NULL);
+    qt_eloop_register_signal(SIGTERM, handle_term, NULL);
 
     /* Bind the service port and register to eloop */
     service_socket = control_socket_init(get_service_port());
     if (service_socket >= 0) {
-        eloop_run();
+        qt_eloop_run();
     } else {
         indigo_logger(LOG_LEVEL_INFO, "Failed to initiate the UDP socket");
     }
 
     /* Stop eloop */
-    eloop_destroy();
+    qt_eloop_destroy();
     indigo_logger(LOG_LEVEL_INFO, "ControlAppC stops");
     if (service_socket >= 0) {
         indigo_logger(LOG_LEVEL_INFO, "Close service port: %d", get_service_port());
